8979e6c219ca3ebc0de0982faad858b37f5635f7
sparkdesignslk <161533000+sparkdesignslk@users.noreply.github.com>
2024-03-01 13:19:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function processDOB() {
+    var dobInput = document.getElementById("dobInput").value;
+    var loading = document.getElementById("loading");
+    var result = document.getElementById("result");
+
+    // Display loading animation
+    loading.style.display = "block";
+
+    //
