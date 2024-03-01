d01b26f486adf8d06689684285fdfff921d1a6da
sparkdesignslk <161533000+sparkdesignslk@users.noreply.github.com>
2024-03-01 13:38:35 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function processDOB() {
-    var dobInput = document.getElementById("dobInput").value;
-    var loading = document.getElementById("loading");
-    var result = document.getElementById("result");
-
-    // Display loading animation
-    loading.style.display = "block";
-
-    //
